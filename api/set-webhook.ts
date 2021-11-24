de33fe44cdf0198e9b9be5850f10225d26d2da0c
Matheus Carnelutt <matheuschafrao@gmail.com>
2021-11-24 19:24:28 -0300

    feat: set-webhook only works on production deployment

--- api/set-webhook.ts
+++ api/set-webhook.ts
@@ -2,6 +2,12 @@ import { createBot, onStart, setWebhook } from '../src/bot';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
+    if (process.env.VERCEL_ENV !== 'production') {
+        return res
+            .status(401)
+            .json({ message: 'Deployment must be production' });
+    }
+
     const bot = createBot();
 
     const { VERCEL_URL, BOT_TOKEN } = process.env;
