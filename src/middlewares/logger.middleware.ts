52e741861aab0449c88ecd797a62ec51e7db4337
Matheus Carnelutt <matheuschafrao@gmail.com>
2021-12-23 02:05:55 -0300

    feat: adding logger middleware

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,12 @@
+import { MiddlewareFn } from 'grammy';
+
+export const LoggerMiddleware = (userIds: number[]): MiddlewareFn => async (ctx, next) => {
+    const message = `[LOGGER] User "${ctx.from?.username}" ran command "${ctx.message?.text}"`;
+
+    // Ignore user sending the command
+    userIds = userIds.filter(userId => ctx.from?.id !== userId);
+
+    await Promise.all(userIds.map(userId => ctx.api.sendMessage(userId, message)));
+
+    await next();
+}
