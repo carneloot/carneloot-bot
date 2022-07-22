746b5e14758165a064349f2e3ebe1dd3267f7250
Matheus Carnelutt <matheuschafrao@gmail.com>
2022-07-22 17:53:05 -0300

    fix: commenting out timesheet module command so help text is not generated

--- src/modules/timesheet/index.ts
+++ src/modules/timesheet/index.ts
@@ -1,15 +1,15 @@
 import { Module } from '../../common/module/module';
-import { AuthMiddleware } from '../../middlewares/auth.middleware';
-
-import { AddTimeCommand } from './add-time.command';
-
-const { TIMESHEET_AUTHORIZED_USERS } = process.env;
+// import { AuthMiddleware } from '../../middlewares/auth.middleware';
+//
+// import { AddTimeCommand } from './add-time.command';
+//
+// const { TIMESHEET_AUTHORIZED_USERS } = process.env;
 
 export const TimesheetModule = new Module(
     'timesheet',
     'Timesheet operations'
 );
 
-const Auth = AuthMiddleware(TIMESHEET_AUTHORIZED_USERS?.split(',') ?? [])
+// const Auth = AuthMiddleware(TIMESHEET_AUTHORIZED_USERS?.split(',') ?? [])
 
-TimesheetModule.setCommand('add', 'Adiciona o horario atual na timesheet', Auth, AddTimeCommand);
+// TimesheetModule.setCommand('add', 'Adiciona o horario atual na timesheet', Auth, AddTimeCommand);
