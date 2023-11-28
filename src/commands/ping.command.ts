bfff7a42757d18c0c1da1d3d3d1ba6ad942ff296
Matheus Carnelutt <carneloot@proton.me>
2023-11-28 10:19:57 -0300

    feat: adding optional delay to ping command

--- src/commands/ping.command.ts
+++ src/commands/ping.command.ts
@@ -1,9 +1,20 @@
+import ms from 'ms';
+
 import type { Command } from '../common/types/command';
+import { sleep } from '../common/utils/sleep';
+
+const MAX_DURATION = ms('10s');
 
 export const PingCommand: Command<'ping'> = {
 	command: 'ping',
 	description: 'Ponga de volta',
 	middleware: () => async (ctx) => {
+		const duration = ctx.match ? ms(ctx.match?.toString()) : null
+
+		if (duration && duration <= MAX_DURATION) {
+			await sleep(duration);
+		}
+
 		await ctx.reply('pong', {
 			reply_to_message_id: ctx.msg?.message_id
 		});
