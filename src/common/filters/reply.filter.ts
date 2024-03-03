e89781c18e6bbcf6f024ad9ab4e7d87da83c1420
Matheus Carnelutt <carneloot@proton.me>
2024-03-02 22:11:56 -0300

    refactor: removing reply filter

--- src/common/filters/reply.filter.ts
+++
@@ -1,7 +0,0 @@
-import { MiddlewareFn } from 'grammy';
-
-export const ReplyFilter: MiddlewareFn = (ctx, next) => {
-	if (ctx.message?.reply_to_message) {
-		return next()
-	}
-}
--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -2,8 +2,7 @@ import { Module } from '../../common/module/module';
 import { Context } from '../../common/types/context';
 import { UserMiddleware } from '../../middlewares/user.middleware';
 import { handleNotificationReply } from './handle-notification-reply';
-import { ReplyFilter } from '../../common/filters/reply.filter';
 
-export const NotificationModule = new Module<Context>('', 'Operações de notificação');
+export const NotificationModule = new Module<Context>('notification', 'Operações de notificação');
 
-NotificationModule.on('message', ReplyFilter, UserMiddleware, handleNotificationReply);
+NotificationModule.on('message', UserMiddleware, handleNotificationReply);
