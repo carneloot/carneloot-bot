16fed8d2653b762a9c825dbc6c81f5cbfb8ae443
Matheus Carnelutt <carneloot@proton.me>
2024-03-05 02:40:36 -0300

    fix: fixing get daily from to timezone math

--- src/common/utils/get-daily-from-to.ts
+++ src/common/utils/get-daily-from-to.ts
@@ -4,20 +4,21 @@ import { zonedTimeToUtc } from 'date-fns-tz';
 import { ConfigValue } from '../../lib/entities/config.js';
 
 export function getDailyFromTo(now: Date, dayStart: ConfigValue<'pet', 'dayStart'>) {
-	let from = set(now, {
-		hours: dayStart.hour,
-		minutes: 0,
-		seconds: 0,
-		milliseconds: 0
-	});
+	let from = zonedTimeToUtc(
+		set(now, {
+			hours: dayStart.hour,
+			minutes: 0,
+			seconds: 0,
+			milliseconds: 0
+		}),
+		dayStart.timezone
+	);
 
 	if (isAfter(from, now)) {
 		from = subDays(from, 1);
 	}
 
-	from = zonedTimeToUtc(from, dayStart.timezone);
-
-	const to = zonedTimeToUtc(addDays(from, 1), dayStart.timezone);
+	const to = addDays(from, 1);
 
 	return { from, to };
 }
