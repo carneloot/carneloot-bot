c30f6c2d1a4b069793120561c09a69ce276be9ee
Matheus Carnelutt <carneloot@proton.me>
2024-07-24 11:18:58 -0300

    fix: fixing hono webhook callback

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,7 @@ if (Env.RUN_MODE === 'webhook') {
 			return c.json({ message: 'Unauthorized' }, 401);
 		}
 
-		webhookCallback(bot, 'hono')(c);
+		await webhookCallback(bot, 'hono')(c);
 	});
 }
 
