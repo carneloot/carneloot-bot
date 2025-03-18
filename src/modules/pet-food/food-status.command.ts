3af16eed29217b92bb76e5f51996ab7b7ad97c41
Matheus Carnelutt <carneloot@proton.me>
2025-03-17 23:04:01 -0300

    feat: food status now shows status for all pets

--- src/modules/pet-food/food-status.command.ts
+++ src/modules/pet-food/food-status.command.ts
@@ -1,4 +1,4 @@
-import { fromUnixTime } from 'date-fns';
+import { fromUnixTime, startOfDay } from 'date-fns';
 import type { MiddlewareFn } from 'grammy';
 
 import Qty from 'js-quantities';
@@ -10,6 +10,7 @@ import { getDailyFromTo } from '../../common/utils/get-daily-from-to.js';
 import { getRelativeTime } from '../../common/utils/get-relative-time.js';
 import { getConfig } from '../../lib/entities/config.js';
 import { getDailyFoodConsumption } from '../../lib/entities/pet-food.js';
+import { getUserCaredPets, getUserOwnedPets } from '../../lib/entities/pet.js';
 
 export const FoodStatusCommand = (async (ctx) => {
 	if (!ctx.user) {
@@ -17,54 +18,49 @@ export const FoodStatusCommand = (async (ctx) => {
 		return;
 	}
 
-	const currentPet = await getConfig('user', 'currentPet', ctx.user.id);
-
-	if (!currentPet) {
-		await ctx.reply(
-			'Você ainda não configurou o pet atual.\nUtilize o comando /escolher_pet para configurar'
-		);
-		return;
-	}
-
-	const dayStart = await getConfig('pet', 'dayStart', currentPet.id);
-
-	if (!dayStart) {
-		await ctx.reply(
-			'Você ainda não configurou o horário de início do dia.\nUtilize o comando /configurar_inicio_dia para configurar'
-		);
-		return;
-	}
-
 	invariant(ctx.message, 'Message object not found.');
 
 	const now = fromUnixTime(ctx.message.date);
 
-	const { from, to } = getDailyFromTo(now, dayStart);
+	const ownedPets = await getUserOwnedPets(ctx.user.id);
+	const caredPets = await getUserCaredPets(ctx.user.id);
 
-	const dailyFoodConsumption = await getDailyFoodConsumption(
-		currentPet.id,
-		from,
-		to
+	const allPets = ownedPets.concat(caredPets);
+
+	const petMessages = await Promise.all(
+		allPets.map(async (pet) => {
+			const dayStart = await getConfig('pet', 'dayStart', pet.id);
+
+			if (!dayStart) {
+				await ctx.reply(
+					'Você ainda não configurou o horário de início do dia.\nUtilize o comando /configurar_inicio_dia para configurar'
+				);
+				return;
+			}
+
+			const { from, to } = getDailyFromTo(now, dayStart);
+
+			const dailyFoodConsumption = await getDailyFoodConsumption(
+				pet.id,
+				from,
+				to
+			);
+
+			const qtd = Qty(dailyFoodConsumption?.total ?? 0, 'g');
+			const lastTime = dailyFoodConsumption
+				? fromUnixTime(dailyFoodConsumption.lastTime)
+				: startOfDay(now);
+			let timeSinceLast = getRelativeTime(lastTime, now, {
+				units: ['years', 'months', 'weeks', 'days', 'hours', 'minutes']
+			});
+
+			if (timeSinceLast.length === 0) {
+				timeSinceLast = 'menos de um minuto';
+			}
+
+			return `- Pet ${pet.name}: ${qtd} há ${timeSinceLast}`;
+		})
 	);
 
-	if (!dailyFoodConsumption || dailyFoodConsumption.total === 0) {
-		await ctx.reply(
-			'Ainda não foi colocado ração hoje. Utilize o comando /colocar_racao'
-		);
-		return;
-	}
-
-	const qtd = Qty(dailyFoodConsumption.total, 'g');
-	const lastTime = fromUnixTime(dailyFoodConsumption.lastTime);
-	let timeSinceLast = getRelativeTime(lastTime, now, {
-		units: ['years', 'months', 'weeks', 'days', 'hours', 'minutes']
-	});
-
-	if (timeSinceLast.length === 0) {
-		timeSinceLast = 'menos de um minuto';
-	}
-
-	await ctx.reply(
-		`Hoje já foram colocados ${qtd} de ração para o pet ${dailyFoodConsumption.name}.\nFoi colocado pela última vez há ${timeSinceLast}.`
-	);
+	await ctx.reply(petMessages.join('\n'), { parse_mode: 'MarkdownV2' });
 }) satisfies MiddlewareFn<Context>;
