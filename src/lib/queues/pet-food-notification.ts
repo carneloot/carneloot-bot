5b0890e6cb61c0e07f22c0e68c3bd25ee1b1eaaa
Matheus Carnelutt <carneloot@proton.me>
2025-03-21 00:47:30 -0300

    refactor: pet food notification processor now uses effect

--- src/lib/queues/pet-food-notification.ts
+++ src/lib/queues/pet-food-notification.ts
@@ -1,6 +1,14 @@
 import { type Processor, Queue, Worker } from 'bullmq';
 
-import { Duration, Effect, Either, Option } from 'effect';
+import {
+	Array as A,
+	Console,
+	Data as D,
+	Duration,
+	Effect,
+	Either,
+	Option
+} from 'effect';
 import { Bot } from 'grammy';
 import Qty from 'js-quantities';
 
@@ -10,6 +18,7 @@ import { Env } from '../../common/env.js';
 import type { Context } from '../../common/types/context.js';
 import { getDailyFromTo } from '../../common/utils/get-daily-from-to.js';
 import { getUserDisplay } from '../../common/utils/get-user-display.js';
+import { DatabaseError } from '../database/error.js';
 import type { PetID } from '../database/schema.js';
 import { getConfig } from '../entities/config.js';
 import { createNotificationHistory } from '../entities/notification.js';
@@ -33,66 +42,80 @@ const queue = new Queue<Data>(QUEUE_NAME, {
 	}
 });
 
-const handler: Processor<Data> = async (job) => {
-	const payload = job.data;
+class MissingPetId extends D.TaggedError('MissingPetId') {}
 
-	const pet = await getPetByID(payload.petID, { withOwner: true });
+class MissingPetDayStart extends D.TaggedError('MissingPetDayStart') {}
 
-	if (!pet) {
-		throw new Error(`Pet ${payload.petID} not found`);
-	}
-
-	const dayStart = await getConfig('pet', 'dayStart', payload.petID);
-
-	if (!dayStart) {
-		throw new Error(`Missing day start for pet ${payload.petID}`);
-	}
-
-	const nowUtc = job.processedOn ? new Date(job.processedOn) : new Date();
-	const { from, to } = getDailyFromTo(nowUtc, dayStart);
-
-	const dailyConsumption = await getDailyFoodConsumption(
-		payload.petID,
-		from,
-		to
+const getDayStart = (petId: PetID) =>
+	Effect.tryPromise({
+		try: () => getConfig('pet', 'dayStart', petId),
+		catch: (err) => new DatabaseError({ cause: err })
+	}).pipe(
+		Effect.andThen(Option.fromNullable),
+		Effect.catchTag('NoSuchElementException', () =>
+			Effect.fail(new MissingPetDayStart())
+		)
 	);
 
-	const carers = await getPetCarers(payload.petID).then((carers) =>
-		carers.filter((carer) => carer.status === 'accepted')
-	);
+const handler: Processor<Data> = (job) =>
+	Effect.gen(function* () {
+		const payload = job.data;
 
-	const bot = new Bot<Context>(Env.BOT_TOKEN);
+		const pet = yield* Effect.tryPromise(() =>
+			getPetByID(payload.petID, { withOwner: true })
+		).pipe(
+			Effect.andThen(Option.fromNullable),
+			Effect.catchTag('NoSuchElementException', () =>
+				Effect.fail(new MissingPetId())
+			)
+		);
 
-	const quantity = Option.map(dailyConsumption, (v) => Qty(v.total, 'g'));
+		const dayStart = yield* getDayStart(payload.petID);
 
-	const message = [
-		`🚨 Hora de dar comida para o pet ${pet.name}.`,
-		Option.match(quantity, {
-			onSome: (q) => `Já foram ${q} hoje.`,
-			onNone: () => 'Ainda não foi dado comida hoje.'
-		})
-	].join(' ');
+		const nowUtc = job.processedOn ? new Date(job.processedOn) : new Date();
+		const { from, to } = getDailyFromTo(nowUtc, dayStart);
 
-	for (const { carer } of [{ carer: pet.owner }, ...carers]) {
-		const sentMessage = await Effect.tryPromise({
-			try: () => bot.api.sendMessage(carer.telegramID, message),
-			catch: (err) =>
-				console.error(
-					`Error sending notification to ${getUserDisplay(carer)}`,
-					err
-				)
-		}).pipe(Effect.either, Effect.runPromise);
+		const dailyConsumption = yield* Effect.tryPromise(() =>
+			getDailyFoodConsumption(payload.petID, from, to)
+		);
 
-		if (Either.isRight(sentMessage)) {
-			await Effect.runPromise(createNotificationHistory({
-				messageID: sentMessage.right.message_id,
-				userID: carer.id,
-				petID: payload.petID,
-				notificationID: null
-			}));
+		const carers = yield* Effect.tryPromise({
+			try: () => getPetCarers(payload.petID),
+			catch: (err) => Console.error('Error getting pet carers', err)
+		}).pipe(Effect.andThen(A.filter((carer) => carer.status === 'accepted')));
+
+		const bot = new Bot<Context>(Env.BOT_TOKEN);
+
+		const quantity = Option.map(dailyConsumption, (v) => Qty(v.total, 'g'));
+
+		const message = [
+			`🚨 Hora de dar comida para o pet ${pet.name}.`,
+			Option.match(quantity, {
+				onSome: (q) => `Já foram ${q} hoje.`,
+				onNone: () => 'Ainda não foi dado comida hoje.'
+			})
+		].join(' ');
+
+		for (const { carer } of [{ carer: pet.owner }, ...carers]) {
+			const sentMessage = yield* Effect.tryPromise({
+				try: () => bot.api.sendMessage(carer.telegramID, message),
+				catch: (err) =>
+					console.error(
+						`Error sending notification to ${getUserDisplay(carer)}`,
+						err
+					)
+			}).pipe(Effect.either);
+
+			if (Either.isRight(sentMessage)) {
+				yield* createNotificationHistory({
+					messageID: sentMessage.right.message_id,
+					userID: carer.id,
+					petID: payload.petID,
+					notificationID: null
+				}).pipe(Effect.ignoreLogged);
+			}
 		}
-	}
-};
+	}).pipe(Effect.runPromise);
 
 export const petFoodNotificationJob = {
 	QUEUE_NAME,
